c5e674f00a1811b0b3283461202023df93a1dbd9
TomYenialem <tom21g2008@gmail.com>
2025-01-05 11:39:38 +0300

    final

--- Db/dbconfig.js
+++ Db/dbconfig.js
@@ -3,21 +3,16 @@ require("dotenv").config(); // Ensure dotenv is loaded first
 
 // Create a connection pool with the provided details
 const dbConnection = mysql.createPool({
-  // host: process.env.HOST, // Database host (IP or hostname)
-  // user: process.env.USER, // Username for the database
-  // password: process.env.PASSWORD, // Password for the database
-  // database: process.env.DATABASE, // Database name
-  // connectTimeout: 10000, // Timeout for connection (in ms)
-
-  user: "freedb_temesgen",
-  database: "freedb_evangadi",
-  host: "sql.freedb.tech",
-  password: "Z??dskcGu4fCwe2",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  connectTimeout: 10000,
-  port: 3306,
+  host:
+    process.env.HOST || "bvqxsbt6n6b1m0fjyywt-mysql.services.clever-cloud.com", // Database host (IP or hostname)
+  user: process.env.USER || "u3vyjzrmrmloszul", // Username for the database
+  password: process.env.PASSWORD || "j7tAQm8Kee3mAvlDPCF1", // Password for the database
+  database: process.env.DATABASE || "bvqxsbt6n6b1m0fjyywt", // Database name
+  waitForConnections: true, // Allows multiple connections in the pool
+  connectionLimit: 10, // Max number of connections in the pool
+  queueLimit: 0, // Unlimited queue size for pending connections
+  connectTimeout: 10000, // Timeout for connection (in ms)
+  port: process.env.DB_PORT || 3306, // Port for MySQL
 });
 
 // Use promises to handle queries
